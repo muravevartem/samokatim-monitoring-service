c61c4a12c0683ea9d773df1afeab8ba40b41a296
muravevas <muravevartem@outlook.com>
2023-01-29 12:34:14 +0400

    Changed endpoint

--- src/main/java/com/muravev/monitoringservice/controllers/TrackController.java
+++ src/main/java/com/muravev/monitoringservice/controllers/TrackController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1/monitoring/track")
+@RequestMapping("/api/v1/monitor/track")
 @RequiredArgsConstructor
 public class TrackController {
     private final TransportService transportService;
