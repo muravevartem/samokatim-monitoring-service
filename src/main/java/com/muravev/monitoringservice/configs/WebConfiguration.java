d9c8565085a6a972050f124dc1cd4883c9df3a1d
muravevas <muravevartem@outlook.com>
2023-01-29 13:47:27 +0400

    Dynamic origins

--- src/main/java/com/muravev/monitoringservice/configs/WebConfiguration.java
+++ src/main/java/com/muravev/monitoringservice/configs/WebConfiguration.java
@@ -1,15 +1,22 @@
 package com.muravev.monitoringservice.configs;
 
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfiguration implements WebFluxConfigurer {
+    @Value("${front.urls}")
+    private List<String> frontUrls;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(frontUrls.toArray(new String[0]))
                 .allowedMethods("PUT", "OPTIONS", "GET", "POST", "PATCH", "DELETE")
                 .allowCredentials(true);
     }
