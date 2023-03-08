c17d064844c860b8b03d8f3e9c9592609945a7de
muravevas <muravevartem@outlook.com>
2023-03-08 18:16:33 +0400

    Removed unused classes

--- src/main/java/com/muravev/monitoringservice/document/TransportStatus.java
+++
@@ -1,7 +0,0 @@
-package com.muravev.monitoringservice.document;
-
-public enum TransportStatus {
-    BUSY,
-    FREE,
-    DISABLED
-}
--- src/main/java/com/muravev/monitoringservice/document/TransportType.java
+++
@@ -1,6 +0,0 @@
-package com.muravev.monitoringservice.document;
-
-public enum TransportType {
-    BICYCLE,
-    SCOOTER
-}
