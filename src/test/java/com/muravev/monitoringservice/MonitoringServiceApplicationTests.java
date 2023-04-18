fb27c5105b809344dca0c6c0aa6d0609b651f1c4
muravevas <muravevartem@outlook.com>
2023-04-18 22:08:22 +0400

    Move to PostgreSQL

--- src/test/java/com/muravev/monitoringservice/MonitoringServiceApplicationTests.java
+++ src/test/java/com/muravev/monitoringservice/MonitoringServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.muravev.monitoringservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DataJpaTest
 class MonitoringServiceApplicationTests {
 
     @Test
