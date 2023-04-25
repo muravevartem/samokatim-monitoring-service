068993c55ed2696c899bf31bfca66b1f3592c932
Artem Muravev <62592488+muravevartem@users.noreply.github.com>
2023-04-25 13:45:47 +0400

    Added Spring Cloud Kubernetes (#5)
    * Added Spring Cloud Kubernetes
    
    * Fixed delay
    
    ---------
    
    Co-authored-by: muravevartem <metra.muravey@gmail.com>

--- src/main/java/com/muravev/monitoringservice/MonitoringServiceApplication.java
+++ src/main/java/com/muravev/monitoringservice/MonitoringServiceApplication.java
@@ -2,11 +2,10 @@ package com.muravev.monitoringservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import java.util.Arrays;
-import java.util.TimeZone;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MonitoringServiceApplication {
 
